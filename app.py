322f1f0369b3255c3f55d5f8bd54d801976e1700
balckwilliam <32457598+balckwilliam@users.noreply.github.com>
2023-06-08 08:49:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,21 @@
-from flask import Flask, render_template_string
-from flask_socketio import SocketIO, emit
-import pexpect
-import eventlet
-
-eventlet.monkey_patch()
+from flask import Flask, request, render_template_string
+import subprocess
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode='eventlet')
-child = pexpect.spawn('/bin/bash')
 
-@socketio.on('input', namespace='/test')
-def test_message(message):
-    command = message['data']
-    child.sendline(command)
-    child.expect('\r\n')
-    result = child.before.decode('utf-8')
-    emit('output', {'data': result})
-
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    output = ""
+    if request.method == 'POST':
+        command = request.form.get('command')
+        try:
+            output = subprocess.check_output(command, shell=True, universal_newlines=True)
+        except Exception as e:
+            output = str(e)
     return render_template_string("""
-        <script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"></script>
-        <script type="text/javascript" charset="utf-8">
-            var socket = io.connect('/test');
-            socket.on('output', function(msg) {
-                document.getElementById('output').value += msg.data + '\\n';
-            });
-            function send_command() {
-                var input = document.getElementById('input');
-                socket.emit('input', {data: input.value});
-                input.value = '';
-            }
-        </script>
-        <textarea id="output" rows="20" cols="80" readonly="readonly"></textarea><br>
-        <input type="text" id="input">
-        <button onclick="send_command()">Send</button>
-    """)
+        <form method="POST">
+            Command: <input type="text" name="command">
+            <input type="submit" value="Run">
+        </form>
+        <pre>{{output}}</pre>
+    """, output=output)
